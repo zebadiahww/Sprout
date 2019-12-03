c08a9bcdf97233c3dffd2d4e9be7df16d60072d0
Darin Marcus Armstrong <darin.marmstrong@gmail.com>
2019-12-03 09:42:50 -0700

    Add createProfileTableViewController & Storyboard along with the photo picker view controller for the profile image

---
+++ Sprout/Sprout/Controllers/View Controllers/PhotoSelectorViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PhotoSelectorViewController.swift
+//  Sprout
+//
+//  Created by Marcus Armstrong on 12/3/19.
+//  Copyright © 2019 Zebadiah Watson. All rights reserved.
+//
+
+import UIKit
+
+protocol PhotoSelectorViewControllerDelegate: class {
+    func photoSelectorViewControllerSelected(image: UIImage)
+}
+
+class PhotoSelectorViewController: UIViewController {
+
+    @IBOutlet weak var selectedPhotoImageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
+    
+    weak var delegate: PhotoSelectorViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        selectImageButton.setTitle("Select Image", for: .normal)
+        selectedPhotoImageView.image = nil
+    }
+    
+    @IBAction func selectImageButtonTapped(_ sender: UIButton) {
+        presentImagePickerActionSheet()
+        selectImageButton.setTitle("", for: .normal)
+    }
+}
+
+extension PhotoSelectorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //creating the function to present the image picker to the user
+    func presentImagePickerActionSheet(){
+        //creating an instance of UIImagePickerController initialized
+        let imagePickerController = UIImagePickerController()
+        //setting the ImagePickerController delegate
+        imagePickerController.delegate = self
+        //creating the action sheet that let's the user select either select a photo or use the camera
+        let actionSheet = UIAlertController(title: "Select a photo or take a picture", message: nil, preferredStyle: .actionSheet)
+        //MARK: - Select a photo from the Library
+        //Here we check if the photoLibrary is available as a source type
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            //if it is available, we add an action to the action sheet titled "Photo" and if it is selected, than the code below will run
+            actionSheet.addAction(UIAlertAction(title: "Photo", style: .default, handler: { (_) in
+                //we set our instance of imagePickerController and set it equal the source type we want: in this case photoLibrary
+                imagePickerController.sourceType = .photoLibrary
+                //we present imagePicker Controller
+                self.present(imagePickerController, animated: true, completion:  nil)
+            }))
+        }
+        //MARK: - Select your camera
+        //Here we check if the camera source type is available
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            //same as above, we are adding an action called "Camera"
+            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (_) in
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(actionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let photo = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectImageButton.setTitle("", for: .normal)
+            selectedPhotoImageView.image = photo
+            delegate?.photoSelectorViewControllerSelected(image: photo)
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
