2a16b747a71f10bc122e88218065c0b32132bc1e
Darin Marcus Armstrong <darin.marmstrong@gmail.com>
2019-12-10 16:21:18 -0700

    adjusts account view to match design flow

--- Sprout/Sprout/Controllers/View Controllers/AccountTableViewController.swift
+++ Sprout/Sprout/Controllers/View Controllers/AccountTableViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class AccountTableViewController: UITableViewController, UITextViewDelegate {
+class AccountTableViewController: UITableViewController, UITextViewDelegate, PhotoSelectorViewControllerDelegate {
+    
+    
     
     // MARK: - Properties
-    
-    @IBOutlet weak var editProfileButton: UIButton!
+
     @IBOutlet weak var photoSelectorView: UIView!
     @IBOutlet weak var editPhotoButton: UIButton!
-    @IBOutlet weak var accountTypeLabel: UILabel!
     @IBOutlet weak var generalNameLabel: UILabel!
     @IBOutlet weak var personalDetailFieldsStackView: UIStackView!
     @IBOutlet weak var nameFieldLabel: UILabel!
@@ -40,12 +40,8 @@ class AccountTableViewController: UITableViewController, UITextViewDelegate {
     @IBOutlet weak var addTagButton: UIButton!
     @IBOutlet weak var tagCollectionView: UICollectionView!
     @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var infoCell: UITableViewCell!
-    @IBOutlet weak var myGoalsLabel: UILabel!
-    @IBOutlet weak var goalsCollectionView: UICollectionView!
-    @IBOutlet weak var seeAllGoalsButton: UIButton!
-    @IBOutlet weak var myMentorsLabel: UILabel!
-    @IBOutlet weak var mentorPupilCollectionView: UICollectionView!
+    
+    var selectedImage: UIImage?
 
     // MARK: - LifeCycle Functions
     
@@ -55,9 +51,7 @@ class AccountTableViewController: UITableViewController, UITextViewDelegate {
     }
     
     // MARK: - Actions
-    
-    @IBAction func editProfileButtonTapped(_ sender: UIButton) {
-    }
+
     
     @IBAction func editPhotoButtonTapped(_ sender: UIButton) {
     }
@@ -73,9 +67,6 @@ class AccountTableViewController: UITableViewController, UITextViewDelegate {
     func initialViewSetup() {
         guard let currentUser = UserController.shared.currentUser else {return}
         photoSelectorView.layer.cornerRadius = photoSelectorView.frame.height/2
-        accountTypeLabel.text = currentUser.isMentor ? "Mentor" : "Pupil"
-        accountTypeLabel.layer.borderWidth = 1
-        accountTypeLabel.layer.borderColor = UIColor.black.cgColor
         generalNameLabel.textAlignment = .left
         generalNameLabel.text = UserController.shared.currentUser?.name
         bioTextView.layer.borderWidth = 1
@@ -86,59 +77,17 @@ class AccountTableViewController: UITableViewController, UITextViewDelegate {
         categoryBorder.layer.borderColor = UIColor.black.cgColor
         tagBorder.layer.borderWidth = 1
         tagBorder.layer.borderColor = UIColor.black.cgColor
-        seeAllGoalsButton.layer.cornerRadius = seeAllGoalsButton.frame.height/2
         saveButton.layer.cornerRadius = saveButton.frame.height/2
     }
     
     func setViewForProfileDefault() {
-        UIView.animate(withDuration: 1, animations: {
-            self.editProfileButton.isHidden = false
-            self.photoSelectorView.frame.size = CGSize(width: self.view.frame.width * 0.4, height: self.view.frame.width * 0.4)
-            self.accountTypeLabel.isHidden = false
-            self.generalNameLabel.textAlignment = .left
-            self.generalNameLabel.text = UserController.shared.currentUser?.name
-            self.personalDetailFieldsStackView.isHidden = true
-            self.bioFieldLabel.isHidden = true
-            self.bioTextView.layer.borderColor = UIColor.clear.cgColor
-            self.InterestsExpertiseLabel.isHidden = true
-            self.pickACategoryLabel.isHidden = true
-            self.categoryBorder.isHidden = true
-            self.tagBorder.isHidden = true
-            self.tagCollectionView.isHidden = true
-            self.addTagButton.isHidden = true
-            self.saveButton.isHidden = true
-            self.infoCell.isHidden = false
-            
-        }) { (success) in
-            if success {
-                print("Successfully animated to default profile view")
-            }
-        }
     }
     
     func setViewForProfileEdit() {
-        UIView.animate(withDuration: 1, animations: {
-            self.editProfileButton.isHidden = true
-            self.photoSelectorView.frame.size = CGSize(width: self.view.frame.width * 0.4, height: self.view.frame.width * 0.4)
-            self.accountTypeLabel.isHidden = true
-            self.generalNameLabel.textAlignment = .center
-            self.generalNameLabel.text = "General"
-            self.personalDetailFieldsStackView.isHidden = false
-            self.bioFieldLabel.isHidden = false
-            self.bioTextView.layer.borderColor = UIColor.black.cgColor
-            self.InterestsExpertiseLabel.isHidden = false
-            self.pickACategoryLabel.isHidden = false
-            self.categoryBorder.isHidden = false
-            self.tagBorder.isHidden = false
-            self.tagCollectionView.isHidden = false
-            self.addTagButton.isHidden = false
-            self.saveButton.isHidden = false
-            self.infoCell.isHidden = true
-            
-        }) { (success) in
-            if success {
-                print("Successfully animated to edit profile view")
-            }
-        }
     }
+    
+    func photoSelectorViewControllerSelected(image: UIImage) {
+        selectedImage = image
+    }
+    
 }
